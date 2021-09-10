ec532184b4a41738424acf850b10e512737f2b1d
koo5 <kolman.jindrich@gmail.com>
2021-09-10 03:23:07 +0200

    seems to be sending stuff in a round-robbin way

--- main.js
+++ main.js
@@ -1,44 +1,126 @@
 CHUNK_SIZE_BYTES = 1000000000; // 1GB
 
-const { spawn } = require('child_process');
 
-const { Command } = require('commander');
+pipe_commands = [
+	'ssh localhost mooltipath join stream1',
+	'ssh localhost mooltipath join stream1',
+];
+
+chunks = [];
+var next_chunk_id = 0;
+drains = [];
+var current_drain_id = 0;
+
+const {spawn} = require('child_process');
+
+const {Command} = require('commander');
 const program = new Command();
 program.version('0.0.1')
 	.command('split')
 	.action(() =>
 	{
-		const proc = spawn('ssh', ['localhost', 'wc', '-c']);
 
-		proc.on('close', (code) => {
-		  console.log(`child process exited with code ${code}`);
+		drains = [create_pipe(), create_pipe()];
+
+		process.stdin.on('data', data =>
+		{
+			console.log(`I got some ${data.length} bytes`);
+			chunks.push({'id': next_chunk_id++, 'data': data});
+			if (try_send_chunks() === false)
+			{
+				console.debug('all drains are busy');
+				if (chunks.length > 100000)
+				{
+					process.stdin.pause();
+					drains.forEach((p) =>
+					{
+						p.once('drain', () =>
+						{
+							process.stdin.resume();
+							try_send_chunks();
+						})
+					})
+				}
+			}
 		});
 
-		proc.stdout.on('data', (data) => {
-		  console.log(`stdout: ${data}`);
+		process.stdin.on('close', () =>
+		{
+			console.log(`This is the end of stdin.`);
+			drains.forEach((p) => p.stdin.end());
 		});
-
-		proc.stderr.on('data', (data) => {
-		  console.error(`stderr: ${data}`);
-		});
-
-		current_pipe = proc.stdin;
-
-		process.stdin.on('data', data => {
-		  console.log(`I got some ${data.length} bytes`);
-		  current_pipe.write(data);
-		});
-
-		process.stdin.on('close', () => {
-		  console.log(`This is the end of stdin.`);
-		  current_pipe.end();
-		});
-
-
 	});
 
 
+function try_send_chunks()
+{
+	let ch = chunks.shift();
+	while (ch != undefined)
+	{
+		if (!try_send_chunk(ch))
+			return false;
+		ch = chunks.shift();
+	}
+	return true;
+}
+
+function try_send_chunk(ch)
+{
+	if (!try_pick_next_free_drain())
+		return false;
+	const pipe = current_drain().stdin;
+	pipe.write(JSON.stringify({'id': ch.id, 'size': ch.data.length}))
+	pipe.write(ch.data);
+	return true;
+}
 
 
-program.
-	parse();
+function try_pick_next_free_drain()
+{
+	tried_pipes = [];
+	while (tried_pipes.length < drains.length)
+	{
+		pick_next_drain();
+		const p = current_drain();
+		if (!p.writableNeedDrain)
+			return true;
+		tried_pipes.push(p);
+	}
+	return false;
+}
+
+function pick_next_drain()
+{
+	current_drain_id += 1;
+	if (current_drain_id > drains.length - 1)
+		current_drain_id = 0;
+}
+
+function current_drain()
+{
+	return drains[current_drain_id];
+}
+
+function create_pipe(cmd)
+{
+	const proc = spawn('ssh', ['localhost', 'wc', '-c']);
+
+	proc.on('close', (code) =>
+	{
+		console.log(`child process exited with code ${code}`);
+	});
+
+	proc.stdout.on('data', (data) =>
+	{
+		console.log(`stdout: ${data}`);
+	});
+
+	proc.stderr.on('data', (data) =>
+	{
+		console.error(`stderr: ${data}`);
+	});
+	return proc;
+}
+
+program.parse();
+
